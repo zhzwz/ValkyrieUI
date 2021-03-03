65b1683558b50d720407090d88faf5bbf4588937
coderzhao <coderzhaoziwei@outlook.com>
2021-03-03 15:30:52 +0800

    Valkyrie

---
+++ source/library/Valkyrie.js
@@ -0,0 +1,54 @@
+import WebSocket from './WebSocket'
+
+const Valkyrie = new Vue({
+  data: {
+    websocket: new WebSocket(),
+    roles: Object(),
+    id: String(),
+    state: String(),
+  },
+  computed: {
+    role() {
+      const id = this.id
+      const role = this.roles[id]
+      const hasId = Boolean(id) && (typeof id === 'string')
+      const hasRole = Boolean(role) && (typeof role === 'object')
+      if (hasId && hasRole) return role
+    },
+    name() {
+      return this.role ? this.role.name : GM_info.script.name
+    },
+    server() {
+      return this.role ? this.role.server : GM_info.script.version
+    },
+    documentTitle() { /* 网页标题同步角色状态 */
+      return `${this.name} ${this.state} ${this.server}`.trim()
+    },
+  },
+  watch: {
+    documentTitle(value) {
+      document.title = value
+    },
+  },
+  methods: {
+    on(type, handler) {
+      return this.websocket.eventEmitter.on(type, handler.bind(this))
+    },
+    once(type, handler) {
+      return this.websocket.eventEmitter.once(type, handler.bind(this))
+    },
+    off(id) {
+      this.websocket.eventEmitter.off(id)
+    },
+    send(command) {
+      this.websocket.onSend(command)
+    },
+    wait(ms) {
+      return new Promise(_ => setTimeout(() => _(), ms))
+    },
+  },
+})
+
+export default Valkyrie
+
+unsafeWindow.Valkyrie = Valkyrie
